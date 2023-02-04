503cdd3d3132222d9394a919183fccfc2feaca90
dh969 <72600772+dh969@users.noreply.github.com>
2023-02-05 03:09:21 +0530

    I am done:

---
+++ app/src/main/java/com/example/babble/SignInActivity.java
@@ -0,0 +1,15 @@
+package com.example.babble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        getSupportActionBar().hide();
+    }
+}
